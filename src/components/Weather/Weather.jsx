e09e94b4dba9b97cac7c793c1daad8e00febc61a
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-04-11 09:33:33 +0300

    complete

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,90 +1,114 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { CiSearch } from "react-icons/ci";
-import { IoSunny } from "react-icons/io5";
-import  humidity  from "../../assets/humidity.png"
-import  windSpeed  from "../../assets/wind_speed.png";
+import clear_icon from "../../assets/clear.png";
+import cloud_icon from "../../assets/cloudy.png";
+import drizzle_icon from "../../assets/drizzle.png";
+import rain_icon from "../../assets/rain.png";
+import snow_icon from "../../assets/snow.png";
+import wind_icon from "../../assets/windy.png";
+import humidity_icon from "../../assets/humidity.png";
 import styles from "./Weather.module.css";
 
 const Weather = () => {
-const [weatherData, setWeatherData] = useState(false);
+  const inputRef = useRef(null);
+  const [weatherData, setWeatherData] = useState(false);
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "11d": wind_icon,
+    "11n": wind_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+    "50d": humidity_icon,
+  };
 
-    const allIcons = {
-        "01d": IoSunny,
-        "01n": IoSunny,
-        "02d": humidity,
-        "02n": humidity,
-        "03d": humidity,
-        "03n": humidity,
-        "04d": humidity,
-        "04n": humidity,
-        "09d": windSpeed,
-        "09n": windSpeed,
-        "10d": windSpeed,
-        "10n": windSpeed,
-        "11d": windSpeed,
-        "11n": windSpeed,
-        "13d": windSpeed,
-        "13n": windSpeed,
-
-
-
+  const searchWeather = async (city) => {
+    if (city === "") {
+      alert("Please enter a city name");
+      return;
     }
 
-    const searchWeather = async (city) => {
-        try {
-          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
-            import.meta.env.VITE_APP_ID
-          }`;
-            
-            const response = await fetch(url);          
-            const data = await response.json();
-            const icon = allIcons[data.weather[0].icon] || IoSunny;
-            console.log(data);
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
 
-            setWeatherData({
-                humidity: data.main.humidity,
-                windSpeed: data.wind.speed,  
-                temperature: Math.floor(data.main.temp),
-                location: data.name,
-                icon: icon
-            })
+      const response = await fetch(url);
+      const data = await response.json();
 
-        } catch (error) {
-            
-        }
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      // console.log(data);
+
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      setWeatherData(false);
+      console.error("Error fetching weather data:", error);
     }
+  };
 
-    useEffect(() => {
-        searchWeather("london");
-    }, []);
+  useEffect(() => {
+    searchWeather();
+  }, []);
 
   return (
     <div className={styles.weather}>
       <div className={styles.searchBar}>
-        <input type="text" placeholder="Search" />
-        <CiSearch size={60} className={styles.searchIcon} />
+        <input ref={inputRef} type="text" placeholder="Search" />
+        <CiSearch
+          onClick={() => searchWeather(inputRef.current.value)}
+          size={60}
+          className={styles.searchIcon}
+        />
       </div>
-      <img src={weatherData.icon} alt= "" className={styles.weatherIcon} />
-          <p className={styles.temperature}>{weatherData.temperature }°C</p>
-          <p className={styles.location}>{weatherData.location }</p>
 
-      <div className={styles.weatherDeta}>
-        <div className={styles.col}>
-          <img src={humidity} alt="" />
-          <div>
-                      <p>{weatherData.humidity }</p>
-            <span>Humidity</span>
-          </div>
-        </div>
+      {weatherData ? (
+        <>
+          <img src={weatherData.icon} alt="" className={styles.weatherIcon} />
 
-        <div className={styles.col}>
-          <img src={windSpeed} alt="" />
-          <div>
-                      <p>{weatherData.windSpeed }</p>
-            <span>Wind Speed</span>
+          <p className={styles.temperature}>{weatherData.temperature}°C</p>
+          <p className={styles.location}>{weatherData.location}</p>
+
+          <div className={styles.weatherDeta}>
+            <div className={styles.col}>
+              <img src={humidity_icon} alt="" />
+              <div>
+                <p>{weatherData.humidity}</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+
+            <div className={styles.col}>
+              <img src={wind_icon} alt="" />
+              <div>
+                <p>{weatherData.windSpeed}</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
